f677e8e07ebde87f5ef2a4b1484012b411880148
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-03-03 23:26:43 +0100

    adding a footer to webpage

--- coffeMachine/resources/views/welcome.blade.php
+++ coffeMachine/resources/views/welcome.blade.php
@@ -66,50 +66,59 @@
         </div>
         <article>
             <h2>1. Espresso</h2>
-            <p>El espresso es un café fuerte y concentrado que se prepara forzando agua caliente a través de granos de
+            <p>El espresso es un café fuerte y concentrado que se prepara forzando agua caliente a través de granos
+                de
                 café finamente molidos. Es la base de muchas otras bebidas de café.</p>
         </article>
 
         <article>
             <h2>2. Cappuccino</h2>
-            <p>El cappuccino es una bebida de café italiana que consiste en partes iguales de espresso, leche vaporizada
+            <p>El cappuccino es una bebida de café italiana que consiste en partes iguales de espresso, leche
+                vaporizada
                 y espuma de leche. Es conocido por su textura cremosa y su capa de espuma.</p>
         </article>
 
         <article>
             <h2>3. Latte</h2>
-            <p>El latte es una bebida de café elaborada con espresso y una cantidad generosa de leche vaporizada. Suele
+            <p>El latte es una bebida de café elaborada con espresso y una cantidad generosa de leche vaporizada.
+                Suele
                 tener una capa de espuma en la parte superior y puede ser aromatizado con jarabes.</p>
         </article>
 
         <article>
             <h2>4. Americano</h2>
-            <p>El americano es una bebida de café que se hace diluyendo el espresso con agua caliente. Tiene un sabor
+            <p>El americano es una bebida de café que se hace diluyendo el espresso con agua caliente. Tiene un
+                sabor
                 más suave que el espresso pero conserva su aroma y sabor distintivos.</p>
         </article>
 
         <article>
             <h2>5. Macchiato</h2>
-            <p>El macchiato es una bebida de café que consiste en espresso con una pequeña cantidad de leche vaporizada
-                o espuma de leche. Su nombre significa "manchado" en italiano, refiriéndose a la mancha de leche en el
+            <p>El macchiato es una bebida de café que consiste en espresso con una pequeña cantidad de leche
+                vaporizada
+                o espuma de leche. Su nombre significa "manchado" en italiano, refiriéndose a la mancha de leche en
+                el
                 espresso.</p>
         </article>
 
         <article>
             <h2>6. Mocha</h2>
-            <p>El mocha es una bebida de café que combina espresso, leche vaporizada, chocolate y crema batida. Es una
+            <p>El mocha es una bebida de café que combina espresso, leche vaporizada, chocolate y crema batida. Es
+                una
                 opción popular para los amantes del chocolate y el café.</p>
         </article>
 
         <article>
             <h2>7. Frappé</h2>
-            <p>El frappé es una bebida de café fría y espumosa hecha con espresso, leche, hielo y jarabes de sabor. Se
+            <p>El frappé es una bebida de café fría y espumosa hecha con espresso, leche, hielo y jarabes de sabor.
+                Se
                 mezcla hasta obtener una consistencia suave y se sirve con crema batida opcional.</p>
         </article>
 
         <article>
             <h2>8. Cold Brew</h2>
-            <p>El cold brew es una bebida de café hecha con granos de café molidos en frío durante un período prolongado
+            <p>El cold brew es una bebida de café hecha con granos de café molidos en frío durante un período
+                prolongado
                 de tiempo. Se sirve frío y tiene un sabor suave y menos ácido que el café caliente.</p>
         </article>
         <div class="reload-rain"></div>
