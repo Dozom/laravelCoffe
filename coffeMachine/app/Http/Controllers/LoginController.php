5274c07d523183a9d7a5fb6a670e8f4b77c08de4
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-03-10 15:38:29 +0100

    Adding some test to validate registration

--- coffeMachine/app/Http/Controllers/LoginController.php
+++ coffeMachine/app/Http/Controllers/LoginController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -17,11 +18,24 @@ class LoginController extends Controller
         //Validar los datos
         $user = new User();
 
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'password' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return ['status' => 1, 'error' => 'Some field is not filled'];
+        }
+
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
-
-        $user->save();
+        try {
+            Log::info(true == $user->save());
+        } catch (\Exception $e) {
+            return ['status' => 1, 'error' => 'Error saving registers'];
+        }
 
         Auth::login($user);
 
--- coffeMachine/tests/Feature/UserTest.php
+++ coffeMachine/tests/Feature/UserTest.php
@@ -9,11 +9,43 @@ use Illuminate\Support\Facades\Artisan;
 
 class UserTest extends TestCase
 {
-    public function test_usertests()
+    use RefreshDatabase;
+    public function test_register_with_empty_fields()
     {
         Artisan::call('migrate');
         // El formulario carga correctamente
-        $carga = $this->get(route('test'));
-        $carga->assertStatus(200)->assertSee('Texto');
+        $response = $this->post(route('validate-register'));
+        $responseData = $response->json();
+        $this->assertEquals($responseData['status'], 1);
+        $this->assertEquals($responseData['error'], 'Some field is not filled');
+        $response->assertStatus(200);
     }
+    public function test_register_with_valid_user()
+    {
+        Artisan::call('migrate');
+        // El formulario carga correctamente
+        $response = $this->post(route('validate-register'), [
+            'name' => 'Dani Nager Carpio',
+            'email' => 'dnager@gmail.com',
+            'password' => 'admin1234'
+        ]);
+        $response->assertStatus(302)->assertSee('Redirecting to');
+    }
+
+    public function test_register_with_existent_user()
+    {
+        $this->test_register_with_valid_user();
+        Artisan::call('migrate');
+        // El formulario carga correctamente
+        $response = $this->post(route('validate-register'), [
+            'name' => 'Dani Nager Carpio',
+            'email' => 'dnager@gmail.com',
+            'password' => 'admin1234'
+        ]);
+        $responseData = $response->json();
+        $this->assertEquals($responseData['status'], 1);
+        $this->assertEquals($responseData['error'], 'Error saving registers');
+        $response->assertStatus(200);
+    }
+
 }
