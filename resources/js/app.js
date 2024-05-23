dfa272af89757c3815e499256016908d79452a02
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-05-23 12:37:32 +0200

    refactor: changed name of function generateQR

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,28 +7,21 @@ console.log('WELCOME MATE');
 function generateQR() {
     let canvas = document.getElementById('canvas')
     let qr = document.getElementById("input_url").value
-    // Call the function
-    fetchData();
 
-    QRCode.toCanvas(canvas, qr.toString(), {width: 200}, function (error) {
+    generateQrCode();
+
+    QRCode.toCanvas(canvas, qr.toString(), { width: 200 }, function (error) {
         if (error) console.error(error)
-        console.log('success!');
+        console.log('QR CODE generated.');
     })
 }
 
-// Function to make a POST request
-function fetchData() {
-    axios.post('/generateQR',{
+function generateQrCode() {
+    axios.post('/generateQR', {
         qr: document.getElementById("input_url").value,
     })
-        .then(response => {
-            // Handle the response data
-            console.log(response.data);
-        })
-        .catch(error => {
-            // Handle any errors
-            console.error('There was an error making the request!', error);
-        });
+        .then(response => console.log(response.data))
+        .catch(error => console.error('There was an error making the request!', error))
 }
 
 document.getElementById("generateQR").addEventListener("click", generateQR);
